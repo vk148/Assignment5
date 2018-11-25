c69b25aa4406e594ac5a852d843d1ed57884c5b5
Kumar, Vijay (vk3768) <vk3768@att.com>
2018-11-25 21:21:58 +0530

    updated test cases

--- src/main/java/com/victor/DevOps/Assignment5/App.java
+++ src/main/java/com/victor/DevOps/Assignment5/App.java
@@ -5,26 +5,36 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class App {
 	@Test
 	public static void main(String[] args) {
-		System.setProperty("webdriver.chrome.driver", "/home/edureka/chromedriver");
+		//System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+		
+		System.setProperty("webdriver.chrome.driver", "C:\\vijay\\Training\\edureka\\Assignments\\Assignment.5\\chromedriver.exe");
 		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setHeadless(true);
+		//chromeOptions.setHeadless(true);
 		WebDriver driver = new ChromeDriver(chromeOptions);
-		System.out.println("Hi, Welcome to Edureka's YouTube Live session on Selenium WebDriver");
-		driver.get("http://localhost:8090/addressbook");
+		System.out.println("Completing Assignment 5");
+		driver.get("http://13.233.75.90:3001/");
+		String x = driver.getTitle();
+	    System.out.println(x);
 		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
-		driver.findElement(By.className("v-button")).click();
-		driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
-		driver.findElement(By.id("gwt-uid-7")).sendKeys("DevOps");
-		driver.findElement(By.id("gwt-uid-9")).sendKeys("9834562709");
-		driver.findElement(By.id("gwt-uid-11")).sendKeys("Devops@edureka.co");
-		driver.findElement(By.id("gwt-uid-13")).sendKeys("01/01/2015");
-		driver.findElement(By.className("v-button-primary")).click(); 
+		//driver.findElement(By.className("v-button")).click();
+		driver.findElement(By.name("login")).sendKeys("user1");
+		driver.findElement(By.name("password")).sendKeys("user1");
+		//driver.findElement(By.id("gwt-uid-9")).sendKeys("9834562709");
+		//driver.findElement(By.id("gwt-uid-11")).sendKeys("Devops@edureka.co");
+		//driver.findElement(By.id("gwt-uid-13")).sendKeys("01/01/2015");
+		driver.findElement(By.name("click")).click(); 
+		System.out.println(driver.findElement(By.name("hello")).getText());
+		Assert.assertTrue(driver.findElement(By.name("hello")).getText().contains("Login Succeeded"));
 		// Thread.sleep(5000); 
-		driver.quit(); 
+		
+		//driver.quit(); 
 	}
 }
--- src/test/java/com/victor/DevOps/Assignment5/AppTest.java
+++ src/test/java/com/victor/DevOps/Assignment5/AppTest.java
@@ -1,5 +1,13 @@
 package com.victor.DevOps.Assignment5;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import junit.framework.Assert;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -31,8 +39,41 @@ public class AppTest
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    public void testAppSuccess()
     {
-        assertTrue( true );
+    	System.setProperty("webdriver.chrome.driver", "C:\\vijay\\Training\\edureka\\Assignments\\Assignment.5\\chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setHeadless(true);
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		System.out.println("Completing Assignment 5");
+		driver.get("http://13.233.75.90:3001/");
+		String x = driver.getTitle();
+	    System.out.println(x);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.name("login")).sendKeys("user1");
+		driver.findElement(By.name("password")).sendKeys("user1");
+		driver.findElement(By.name("click")).click(); 
+		System.out.println(driver.findElement(By.name("hello")).getText());
+		Assert.assertTrue(driver.findElement(By.name("hello")).getText().contains("Login Succeeded"));
+		driver.quit();
+    }
+    
+    public void testAppFail()
+    {
+    	System.setProperty("webdriver.chrome.driver", "C:\\vijay\\Training\\edureka\\Assignments\\Assignment.5\\chromedriver.exe");
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.setHeadless(true);
+		WebDriver driver = new ChromeDriver(chromeOptions);
+		System.out.println("Completing Assignment 5");
+		driver.get("http://13.233.75.90:3001/");
+		String x = driver.getTitle();
+	    System.out.println(x);
+		driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+		driver.findElement(By.name("login")).sendKeys("user1");
+		driver.findElement(By.name("password")).sendKeys("user2");
+		driver.findElement(By.name("click")).click(); 
+		System.out.println(driver.findElement(By.name("hello")).getText());
+		Assert.assertTrue(driver.findElement(By.name("hello")).getText().contains("Login Failed!"));
+		driver.quit();
     }
 }
